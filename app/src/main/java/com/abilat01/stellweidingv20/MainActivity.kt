8392c1c1331a0625b73e3e94a6ae37d311c94123
Abilat01 <abilat01@yandex.ru>
2020-08-05 15:33:50 +0300

    header + Profiles

--- app/src/main/java/com/abilat01/stellweidingv20/MainActivity.kt
+++ app/src/main/java/com/abilat01/stellweidingv20/MainActivity.kt
@@ -2,10 +2,68 @@ package com.abilat01.stellweidingv20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.abilat01.stellweidingv20.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mDraver: Drawer
+    private lateinit var mHeader: AccountHeader
+    private lateinit var mToolbar: androidx.appcompat.widget.Toolbar
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
     }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+    }
+
+    private fun initFunc() {
+        setSupportActionBar(mToolbar)
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+        mDraver = DrawerBuilder()
+            .withActivity(this)
+            .withToolbar(mToolbar)
+            .withActionBarDrawerToggle(true) //боковая кнопка
+            .withSelectedItem(-1) //выбор меню
+            .withAccountHeader(mHeader) //хеадер
+            .addDrawerItems(
+                PrimaryDrawerItem().withIdentifier(100)
+                    .withIconTintingEnabled(true)
+                    .withName("Программы")
+                    .withSelectable(false)
+            ).build()
+    }
+
+    private fun createHeader() {
+        mHeader = AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem().withName("Пацанская гиря")
+            ).build()
+    }
+
+
+    private fun initFields() {
+        mToolbar = mBinding.mainToolbar
+    }
+
+
 }
